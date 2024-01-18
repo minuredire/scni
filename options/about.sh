2a346f50a65f84678083f247868c6da0b100de89
joko wahyudi <142997824+minuredire@users.noreply.github.com>
2024-01-18 18:32:12 +0700

    Add files via upload

---
+++ options/about.sh
@@ -0,0 +1,26 @@
+ echo -e "    
+
+figlet JOYS TUNNELING  
+                                                                         
+                                                                         
+ " | lolcat
+
+
+echo -e "┌─────────────────────────────────────────────────┐" | lolcat
+echo -e "│               AUTOSCRIPT PREMIUM                │" | lolcat
+echo -e "│             JOYS VPN STORE              │" | lolcat
+echo -e "└─────────────────────────────────────────────────┘" | lolcat
+
+echo -e "> Support OS 
+  For Ubuntu 18.04 & Ubuntu 20.04 & Ubuntu 22.04 
+  For Debian 9 & Debian 10 & Debian 11 " | lolcat
+echo ""
+echo -e "  Contact Owner AutoScript
+> WA: 081329322508
+> TELEGRAM: t.me/dilanjaya" | lolcat
+echo ""
+echo -e "Thanks for your Order ILHAM STORE" | lolcat
+echo ""
+read -n 1 -s -r -p "  Press any key to Back to Menu"
+menu
+fi
